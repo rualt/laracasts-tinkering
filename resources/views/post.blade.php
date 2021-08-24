e5bcf7c2332f1a597910366b65c9cab667cf6694
rualt <rualnar@gmail.com>
2021-08-24 22:13:27 +0400

    Add initial changes for static blog

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+    <title>Test Title</title>
+    <link rel="stylesheet" href="/app.css">
+
+    <body>
+        <article>
+            <h1>{{$post->title}}</h1>
+            <div>{{$post->body}}</div>
+        </article>
+        <h2><a href="/">Back</a></h2>
+    </body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+    <title>Test Title</title>
+    <link rel="stylesheet" href="/app.css">
+
+    <body>
+        @foreach ($posts as $post)
+            <article>
+                <a href="posts/{{$post->slug}}">
+                    <h1>{{$post->title}}</h1>
+                </a>
+                <div>{{$post->excerpt}}</div>
+            </article>
+        @endforeach
+    </body>
