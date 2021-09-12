fd50ecb80a93d748e1518a351e9ac8f7962995b8
rualt <rualnar@gmail.com>
2021-09-12 11:31:06 +0400

    Create Post and Category factory, remove hardcoded seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Category;
 use App\Models\Post;
 
 use Illuminate\Database\Seeder;
@@ -18,58 +17,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
+        // php artisan migrate:fresh --seed
 
-        $user = User::factory()->create();
-
-        $personal_category = Category::create([
-            'name' => 'Pesonal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $family_category = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work_category = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
+        Post::factory(5)->create([
+            //assign all 5 post to single user
             'user_id' => $user->id,
-            'category_id' => $personal_category->id,
-            'title' => 'My Personal post',
-            'slug' => 'my-personal-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                Hic praesentium est tempore odio illum consequuntur accusantium nobis quae,
-                dicta dolore ducimus, laborum ut reprehenderit consequatur facere architecto velit sed quas!',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family_category->id,
-            'title' => 'My Family post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                Hic praesentium est tempore odio illum consequuntur accusantium nobis quae,
-                dicta dolore ducimus, laborum ut reprehenderit consequatur facere architecto velit sed quas!',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work_category->id,
-            'title' => 'My Work post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                Hic praesentium est tempore odio illum consequuntur accusantium nobis quae,
-                dicta dolore ducimus, laborum ut reprehenderit consequatur facere architecto velit sed quas!',
         ]);
     }
 }
